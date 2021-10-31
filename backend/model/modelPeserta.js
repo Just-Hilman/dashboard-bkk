c9f0e88531fff39445540caa2366e266dc4fd364
Just-Hilman <hilman620@yahoo.com>
2021-10-31 07:57:44 +0700

    Create modelPeserta.js

---
+++ backend/model/modelPeserta.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+
+const DataTypes = Sequelize;
+
+const Loker = db.define('peserta_seleksi', {
+    nama: {
+        type: DataTypes.STRING
+    },
+    alamat: {
+        type: DataTypes.STRING
+    },
+    tempat_tgl_lahir: {
+        type: DataTypes.DATE
+    },
+    jurusan: {
+        type: DataTypes.STRING
+    },
+    no_tep: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    id_loker: {
+        type: DataTypes.INT
+    }
+}, {
+    freezeTableName: true
+});
