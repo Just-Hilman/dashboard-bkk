931023713396c9e0f8e173594301e8a14c221c8f
Just-Hilman <hilman620@yahoo.com>
2021-10-31 10:06:27 +0700

    aded update rule for peserta

--- backend/controller/Peserta.js
+++ backend/controller/Peserta.js
@@ -10,11 +10,41 @@ export const getAllPeserta = async (req, res) => {
 
 }
 
+export const getPesertaById = async (req, res) => {
+    try {
+        const peserta = await Peserta.findAll({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json(peserta[0]);
+    } catch (error) {
+        res.json({ messege: error.messege });
+    }
+
+}
+
 export const createPeserta = async (req, res) => {
     try {
         await Peserta.create(req.body);
         res.json({
             "messege": "Peserta Added"
+        });
+    } catch (error) {
+        res.json({ messege: error.messege });
+    }
+
+}
+
+export const updatePeserta = async (req, res) => {
+    try {
+        await Peserta.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json({
+            "messege": "Peserta Updated"
         });
     } catch (error) {
         res.json({ messege: error.messege });
--- backend/routes/routePeserta.js
+++ backend/routes/routePeserta.js
@@ -2,12 +2,16 @@ import express from "express";
 
 import {
     getAllPeserta,
-    createPeserta
+    createPeserta,
+    getPesertaById,
+    updatePeserta
 } from "../controller/Peserta.js";
 
 const router = express.Router();
 
 router.get('/', getAllPeserta);
+router.get('/:id', getPesertaById);
 router.post('/', createPeserta);
+router.patch('/:id', updatePeserta);
 
 export default router;
