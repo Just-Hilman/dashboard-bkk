7248c3a58a1c2e36474e5146789c09c118dcc93b
Just-Hilman <hilman620@yahoo.com>
2021-11-06 12:03:57 +0700

    Update routeUsers.js

--- backend/routes/routeUsers.js
+++ backend/routes/routeUsers.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUsers, Register } from "../controller/Users.js";
+import { verifyToken } from "../midelware/VerifyToken.js";
 
 const router = express.Router();
 
-router.get('/', getUsers);
+router.get('/', verifyToken, getUsers);
 router.post('/', Register);
 
 export default router;
