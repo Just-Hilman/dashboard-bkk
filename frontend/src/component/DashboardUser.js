e0e0c7afc85c8615b7fe3a8cb63daf13583ec86f
Just-Hilman <hilman620@yahoo.com>
2021-11-09 23:30:10 +0700

    updated dashboard

--- frontend/src/component/DashboardUser.js
+++ frontend/src/component/DashboardUser.js
@@ -28,9 +28,88 @@ const DashboardUser = () => {
     }
 
     return (
-        <div>
-            <h1>Selamat Datang: { name }</h1>
-        </div>
+        <body id="page-top">
+            <div id="wrapper">
+                
+                <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div class="sidebar-brand-icon rotate-n-15">
+                            <i class="fas fa-laugh-wink"></i>
+                        </div>
+                        <div class="sidebar-brand-text mx-3">BKK SMKN 6</div>
+                    </a>
+
+                    <hr class="sidebar-divider my-0"></hr>
+
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-tachometer-alt"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+
+
+                    <hr class="sidebar-divider"></hr>
+
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                            aria-expanded="true" aria-controls="collapseTwo">
+                            <i class="fas fa-fw fa-cog"></i>
+                            <span>Lowongan Pekerjaan</span>
+                        </a>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <a class="collapse-item" href="buttons.html">Daftar Lowongan Pekrejaan</a>
+                                <a class="collapse-item" href="cards.html">Cards</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                            aria-expanded="true" aria-controls="collapseUtilities">
+                            <i class="fas fa-fw fa-wrench"></i>
+                            <span>Seleksi</span>
+                        </a>
+                        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                            data-parent="#accordionSidebar">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <a class="collapse-item" href="utilities-color.html">Jadwal Seleksi</a>
+                                <a class="collapse-item" href="utilities-border.html">Peserta Seleksi</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <hr class="sidebar-divider"></hr>
+
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                            aria-expanded="true" aria-controls="collapsePages">
+                            <i class="fas fa-fw fa-folder"></i>
+                            <span>Laporan</span>
+                        </a>
+                        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <a class="collapse-item" href="login.html">Rekap Harian</a>
+                                <a class="collapse-item" href="register.html">Rekap Bulanan</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <hr class="sidebar-divider d-none d-md-block"></hr>
+
+                    <div class="sidebar-card d-none d-lg-flex">
+                        <p class="text-center mb-2"><strong>BKK SMKN 6 Dashboard</strong> is created and distributed by Hilman</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                        </svg>
+                    </div>
+                </ul>
+            </div>
+        </body>
+        // <div>
+        //         <h1>Selamat Datang: { name }</h1>
+        // </div>
     )
 }
 
--- frontend/src/view/MainPage.js
+++ frontend/src/view/MainPage.js
@@ -2,138 +2,136 @@ import React from 'react'
 
 const MainPage = () => {
     return (
-        <html lang="en">
-            <body>
-                <header>
-                    {/* Navbar */}
-                    <nav className="navbar fixed-top navbar-expand-lg navbar-light">
-                        <div className="container">
-                            <a className="navbar-brand page-scroll" href="#home">BKK SMKN 6</a>
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav mx-auto">
-                                    <li className="nav-item active">
-                                        <a className="nav-link page-scroll" href="#home">Home <span className="sr-only">(current)</span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link page-scroll" href="#loker">Lowongan Kerja <span></span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link page-scroll" href="#contact">Contact Us <span></span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link page-scroll" href="#faq">FAQ <span></span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link page-scroll" href="#about">About <span></span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="http://localhost:3000/login">
-                                            <button className="btn tombol-login" >
-                                                <svg width="30px" height="30px" viewBox="0 0 16 16" className="bi bi-person"
-                                                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                    <path fillRule="evenodd"
-                                                        d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                                </svg>
-                                            </button>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+        <body>
+            <header>
+                {/* Navbar */}
+                <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+                    <div className="container">
+                        <a className="navbar-brand page-scroll" href="#home">BKK SMKN 6</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav mx-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link page-scroll" href="#home">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link page-scroll" href="#loker">Lowongan Kerja <span></span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link page-scroll" href="#contact">Contact Us <span></span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link page-scroll" href="#faq">FAQ <span></span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link page-scroll" href="#about">About <span></span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="http://localhost:3000/login">
+                                        <button className="btn tombol-login" >
+                                            <svg width="30px" height="30px" viewBox="0 0 16 16" className="bi bi-person"
+                                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fillRule="evenodd"
+                                                    d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
-                    </nav>
-                    {/* End Of Navbar */}
+                    </div>
+                </nav>
+                {/* End Of Navbar */}
 
-                    {/* Jumbotron */}
-                    <div className="jumbotron">
-                        <div className="text-center">
-                            <h1>Selamat Datang !</h1>
-                            <h2>Di Portal Bursa Kerja Khusus SMKN 6 Bandung</h2>
-                            <p>Have something great in mind? Feel free to contact me. <br></br>
-                                I'll help you to make it happen.</p>
-                        </div>
-                            
-                        <div className="cta">
-                            <a href="http://localhost:3000/loker" className="jumbotron-button">Lihat Lowongan</a>
-                        </div>
-                        <div className="color-overlay"></div>
+                {/* Jumbotron */}
+                <div className="jumbotron">
+                    <div className="text-center">
+                        <h1>Selamat Datang !</h1>
+                        <h2>Di Portal Bursa Kerja Khusus SMKN 6 Bandung</h2>
+                        <p>Have something great in mind? Feel free to contact me. <br></br>
+                            I'll help you to make it happen.</p>
+                    </div>
+                        
+                    <div className="cta">
+                        <a href="http://localhost:3000/loker" className="jumbotron-button">Lihat Lowongan</a>
+                    </div>
+                    <div className="color-overlay"></div>
+                </div>
+
+                {/* End Of Jumbotron */}
+            </header>
+
+            <main>
+                <div className="my-content">
+                    <div className="highlight-text">
+                        <h2>How Can I Help You?</h2>
+                        <h5>Our work then targeted, best practices outcomes social innovation synergy. <br></br>
+                            Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
+                            program areas scale.
+                        </h5>
                     </div>
 
-                    {/* End Of Jumbotron */}
-                </header>
+                    <article id="consult" className="card">
+                        <h5>Consult</h5>
+                        <p>Co-create, design thinking; strengthen infrastructure resist granular.
+                            Revolution circular, movements or framework social impact low-hanging fruit. 
+                            Save the world compelling revolutionary progress.
+                        </p>
+                    </article>
+                        
+                    <article id="design" className="card">
+                        <h5>Design</h5>
+                        <p>Policymaker collaborates collective impact humanitarian shared value
+                            vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile 
+                            issue outcomes vibrant boots on the ground sustainable.
+                        </p>
+                    </article>
+                        
+                    <article id="develop" className="card">
+                        <h5>Develop</h5>
+                        <p>Revolutionary circular, movements a or impact framework social impact low-
+                            hanging. Save the compelling revolutionary inspire progress. Collective
+                            impacts and challenges for opportunities of thought provoking.
+                        </p>
+                    </article>
+                        
+                    <article id="marketing" className="card">
+                        <h5>Marketing</h5>
+                        <p>Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile,
+                            replicable, effective altruism youth. Mobilize commitment to overcome
+                            injustice resilient, uplift social transparent effective.
+                        </p>
+                    </article>
+                        
+                    <article id="manage" className="card">
+                        <h5>Manage</h5>
+                        <p>Change-makers innovation or shared unit of analysis. Overcome injustice
+                            outcomes strategize vibrant boots on the ground sustainable. Optimism,
+                            effective altruism invest optimism corporate social.
+                        </p>
+                    </article>
+                        
+                    <article id="evolve" className="card">
+                        <h5>Evolve</h5>
+                        <p>Activate catalyze and impact contextualize humanitarian. Unit of analysis
+                            overcome injustice storytelling altruism. Thought leadership mass 
+                            incarceration. Outcomes big data, fairness, social game-changer.
+                        </p>
+                    </article>
 
-                <main>
-                    <div className="my-content">
-                        <div className="highlight-text">
-                            <h2>How Can I Help You?</h2>
-                            <h5>Our work then targeted, best practices outcomes social innovation synergy. <br></br>
-                                Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
-                                program areas scale.
-                            </h5>
-                        </div>
+                    <div className="clear"></div>
 
-                        <article id="consult" className="card">
-                            <h5>Consult</h5>
-                            <p>Co-create, design thinking; strengthen infrastructure resist granular.
-                                Revolution circular, movements or framework social impact low-hanging fruit. 
-                                Save the world compelling revolutionary progress.
-                            </p>
-                        </article>
-                            
-                        <article id="design" className="card">
-                            <h5>Design</h5>
-                            <p>Policymaker collaborates collective impact humanitarian shared value
-                                vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile 
-                                issue outcomes vibrant boots on the ground sustainable.
-                            </p>
-                        </article>
-                            
-                        <article id="develop" className="card">
-                            <h5>Develop</h5>
-                            <p>Revolutionary circular, movements a or impact framework social impact low-
-                                hanging. Save the compelling revolutionary inspire progress. Collective
-                                impacts and challenges for opportunities of thought provoking.
-                            </p>
-                        </article>
-                            
-                        <article id="marketing" className="card">
-                            <h5>Marketing</h5>
-                            <p>Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile,
-                                replicable, effective altruism youth. Mobilize commitment to overcome
-                                injustice resilient, uplift social transparent effective.
-                            </p>
-                        </article>
-                            
-                        <article id="manage" className="card">
-                            <h5>Manage</h5>
-                            <p>Change-makers innovation or shared unit of analysis. Overcome injustice
-                                outcomes strategize vibrant boots on the ground sustainable. Optimism,
-                                effective altruism invest optimism corporate social.
-                            </p>
-                        </article>
-                            
-                        <article id="evolve" className="card">
-                            <h5>Evolve</h5>
-                            <p>Activate catalyze and impact contextualize humanitarian. Unit of analysis
-                                overcome injustice storytelling altruism. Thought leadership mass 
-                                incarceration. Outcomes big data, fairness, social game-changer.
-                            </p>
-                        </article>
+                </div>
+            </main>
 
-                        <div className="clear"></div>
-
-                    </div>
-                </main>
-
-                <footer>
-                    <p> &#169; 2021 Hilman. All Right Reserved.</p>
-                </footer>
-            </body>
-        </html>
-    )
+            <footer>
+                <p> &#169; 2021 Hilman. All Right Reserved.</p>
+            </footer>
+        </body>
+)
 }
 
 export default MainPage
